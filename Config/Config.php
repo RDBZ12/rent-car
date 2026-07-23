3fdb0a7438c0bc7097b446876e003b075c5ef6ae
RDBZ12 <rdbz12@users.noreply.github.com>
2026-07-23 14:29:58 -0400

    Auto-detect Vercel URL if BASE_URL is not set

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,15 @@
 <?php
-$baseUrl = getenv('BASE_URL') ? getenv('BASE_URL') : "http://localhost/alquiler/";
+$baseUrl = getenv('BASE_URL');
+if (!$baseUrl) {
+    if (getenv('VERCEL_URL')) {
+        $baseUrl = "https://" . getenv('VERCEL_URL') . "/";
+    } else {
+        $baseUrl = "http://localhost/alquiler/";
+    }
+}
+if (substr($baseUrl, -1) !== '/') {
+    $baseUrl .= '/';
+}
 $dbHost  = getenv('DB_HOST')  ? getenv('DB_HOST')  : "localhost";
 $dbUser  = getenv('DB_USER')  ? getenv('DB_USER')  : "root";
 $dbPass  = getenv('DB_PASS')  !== false ? getenv('DB_PASS')  : "";
